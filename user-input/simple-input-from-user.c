0ffd5eba4b60b883ccdfd2be1544ceaeccb2f4b2
sudheer562 <cool.sudheer562@gmail.com>
2017-07-13 07:11:12 +0530

    new file simple-input-from-user.c is added to take a number from user as a input and print it as it is

---
+++ user-input/simple-input-from-user.c
@@ -0,0 +1,13 @@
+//author :- Sudheer Kumar Daram
+//RGUKTN
+// This is a program which will take a single input from user and print it to screen using scanf built in function
+#include<stdio.h>
+int main(){
+	
+	int number;
+	// scanf() is a built in function in c which is used to take input from user mostly
+	scanf("%d",&number);
+	printf("\n The number you had enter is %d",number);
+	return 0;
+	
+}
